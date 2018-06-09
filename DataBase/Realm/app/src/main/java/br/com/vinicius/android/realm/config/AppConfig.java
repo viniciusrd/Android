1c2f45d1581a9edd62cf20daa0bd76db7143e6c9
Vinícius Rodrigues <vinird1993@gmail.com>
2018-06-09 14:00:50 -0300

    Create Project

---
+++ DataBase/Realm/app/src/main/java/br/com/vinicius/android/realm/config/AppConfig.java
@@ -0,0 +1,11 @@
+package br.com.vinicius.android.realm.config;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class AppConfig extends Application {
+
+
+}
